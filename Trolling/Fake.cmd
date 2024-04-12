18425cb05f42d91fcb4f9995ce2ab144f3cb42ef
Johann Kramer <129892575+Johann-FullHD@users.noreply.github.com>
2024-04-12 22:49:12 +0200

    A fake hacking prank

---
+++ Trolling/Fake.cmd
@@ -0,0 +1,95 @@
+@echo off
+cls
+color a
+:top
+echo Are you aware of the fact that you are using a virus?
+pause
+cls
+echo () . W - Fbi%20Database/file E2 Mod - - - - -()(.( 
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo ifelse-goqw
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo felt- -100%Would1you1like1to1login!?!?!?
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo when - 2 go start Loadout start LOk
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo %()protocol=starting
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo Index begin ()
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo RAW-PACKET
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo Begin to load the 12a1sf
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo Fbi Database Emulator index complete
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo begin protocol-?=unknowngateway GOTO 1 to 1 indPktlens to fbi
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo.
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo begin Extraction+101;typeA102131013
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo %random% packet type %random% 
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+pause
+echo Begin ProtocolA12WAGstartup direct\start
+echo startup Protcol Complete. Begining starter
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+pause
+cls
+:menutop
+echo () . W - protocol-typeoutontoFBIWHEAL=you
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo ifotherthan-=beginNOTfo
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo felt- -100%awaq ??!3
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo when - 2 go start M0dProtonVPN-SEC100=typeout-onto?=FBIdatabaseDEL - crim
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo %()protocol=starting AWAW
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo Index beginning SHUTDOWN/t=-10-9=1countdown = true?
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo RAW-PACKET
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo Begin to load the 12a1sf
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo Fbi Database Emulator index complete
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo begin protocol-?=unknowngateway GOTO 1 to 1 indPktlens to fbi
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo DEA Fbi StartHack
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo begin Extraction+101;typeA102131013
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo %random% packet type %random% %random%
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo Begin ProtocolA12WAGstartup direct\start
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo startup Prot
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo locate all possible agents
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo.
+echo  System scan complete.
+echo  Searching for vulnerabilities...
+for /l %%i in (1,1,5) do (
+  echo  Searching %%i/%...
+  ping -n 2 localhost >nul
+)
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo  No vulnerabilities found.
+echo
+echo.
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo  System files accessed. Extracting data...
+for /l %%i in (1,1,10) do (
+  echo  Extracting %%i/%...
+  ping -n 1 localhost >nul
+)
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo  Data transfer complete.
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+echo █▀▄▀█ ─█▀█─ █▄─█─ █▄─█
+PING 1.1.1.1 -n 1 -w 1.0 >NUL
+goto :menutop
